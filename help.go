fc9912c86d28564bfd77aa67f36eb29bd0c940bb
js <js@qq.com>
2024-08-20 15:23:58 +0800

    add help

---
+++ help.go
@@ -0,0 +1,36 @@
+package gcurl
+
+import (
+	"strings"
+)
+
+// 字符串分割转切片
+func AllowFileExtStr2Slice(allowExt string) []string {
+	temp := strings.Split(allowExt, "|")
+	exts := make([]string, len(temp))
+	i := 0
+	for _, item := range temp {
+		if item != "" {
+			exts[i] = item
+			i++
+		}
+	}
+	return exts
+}
+
+// 判断是否允许的文件类型,不区分大小写，ext=扩展名，allowExt支持的扩展名如png|jpg|jpeg|gif|txt|doc|docx|pdf|mp4
+func IsAllowFileExt(ext string, allowExt string) bool {
+	if strings.HasPrefix(ext, ".") {
+		ext = string(ext[1:])
+	}
+	exts := AllowFileExtStr2Slice(allowExt)
+	for _, item := range exts {
+		if item == "*" {
+			return true
+		}
+		if strings.EqualFold(item, ext) {
+			return true
+		}
+	}
+	return false
+}
